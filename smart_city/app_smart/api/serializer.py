e190d629788e92149c97308c455dfcf4e5e30a03
Daniel de Souza Alves <53505046817@>
2024-06-07 11:25:40 -0300

    first commit

---
+++ smart_city/app_smart/api/serializer.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+
+from ..models import ContadorData, Sensor, TemperaturaData
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    password = serializers.CharField(write_only=True)
+    
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+    
+    
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+        
+class SensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = '__all__'
+        
+class TemperaturaDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TemperaturaData
+        fields = '__all__'
+        
+class ContadorDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContadorData
+        fields = '__all__'
+        
